cc88a3f4ade3e5855dc424bbe6c7cc0a147bbe0e
israzahid <59427634+israzahid@users.noreply.github.com>
2021-01-22 09:41:14 -0500

    Delete date.js

--- date.js
+++
@@ -1,9 +0,0 @@
-exports.getDay = function () {
-    const today = new Date();
-
-    const options = {
-      weekday: "long",
-    };
-    
-    return today.toLocaleDateString("en-US", options);
-};
